aed5921913b31287f1ea9bfb319bcd11686ef341
wangbingyi <1041493283@qq.com>
2022-03-22 22:51:56 +0800

    update

--- 00~CODE/demo.py
+++ 00~CODE/demo.py
@@ -1,6 +1,14 @@
-#%%
-import requests
+import time
 
+target = 100
+t = 0.2
 
-#%%
+for i in range(100):
+    print('#' * i, end='')
+    print(' ' * (target-i), end='')
+    print(f':剩余时间：{(target - i) * t} 秒', end='')
+    print('\r', end='')
+    time.sleep(t)
 
+print('#' * target, end='')
+print(f':已完成', end='')
